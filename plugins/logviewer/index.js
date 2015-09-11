3e8f6fe3e6fede7078052351c526d3c641b80073
hefangshi <hefangshi@gmail.com>
2015-09-11 15:24:35 +0800

    [new] support cat log

--- plugins/logviewer/index.js
+++ plugins/logviewer/index.js
@@ -1,6 +1,6 @@
 var getArgv = require('../../lib/util.js').getArgv;
 var PORT = getArgv('port');
-PORT = parseInt(PORT.replace('$PORT', process.env.port), 10) + parseInt(getArgv('portOffset'), 10);
+PORT = parseInt(PORT.replace('$PORT', process.env.port || process.env.PORT), 10) + parseInt(getArgv('portOffset'), 10);
 var LOGS = JSON.parse(getArgv('logs'));
 var ROOT_PATH = getArgv('root_path');
 var strftime = require('fast-strftime');
@@ -8,6 +8,8 @@ var ts = require('tail-stream');
 var fs = require('fs');
 var path = require('path');
 var io = require('socket.io')(PORT);
+var byline = require('byline');
+var async = require('async');
 console.log('[yog2-agent] logview server is listening to', PORT);
 
 var logMap = LOGS.reduce(function (prev, current) {
@@ -21,13 +23,13 @@ io.on('connection', function (socket) {
     console.log('[yog2-agent] logview client connected');
 
     socket.on('cat', function (message) {
-        console.log('[yog2-agent] client request log');
-        catLog(message.name, message.from, message.to, message.grep, message.excludeGrep, socket);
+        console.log('[yog2-agent] client request cat log');
+        catLog(message.name, message.from, message.to, message.grep, message.excludeGrep, message.eventID, socket);
     });
 
     socket.on('tail', function (message) {
-        console.log('[yog2-agent] client request log');
-        var stream = tailNewestLog(message.name, message.grep, message.excludeGrep, socket);
+        console.log('[yog2-agent] client request tail log');
+        var stream = tailNewestLog(message.name, message.grep, message.excludeGrep, message.eventID, socket);
         if (!stream) {
             socket.emit('notfound', {
                 cmd: 'tail',
@@ -51,11 +53,84 @@ io.on('connection', function (socket) {
     });
 });
 
-function catLog(name, from, to, grep, socket) {
-
+function catLog(name, from, to, grep, excludeGrep, eventID, socket) {
+    from = new Date(from);
+    to = new Date(new Date(to) + 60 * 1000);
+    var conf = logMap[name];
+    var files = getFilesBetweenTime(conf.path, from, to);
+    var fromPattern = strftime(conf.timePattern, from);
+    var toPattern = strftime(conf.timePattern, to);
+    var total = 0;
+    var allDone = false;
+    var content = '';
+    async.eachSeries(files, function (file, cb) {
+        if (allDone) {
+            return cb && cb(null);
+        }
+        grepFile(file, fromPattern, toPattern, grep, excludeGrep, function (err, data) {
+            if (err) {
+                return cb && cb(null);
+            }
+            total += data.length;
+            if (total > 5000) {
+                allDone = true;
+            }
+            content += data.slice(0, 5000).join('\n');
+            return cb && cb(null);
+        });
+    }, function (err) {
+        console.log('[yog2-agent] grep log end');
+        socket.emit(eventID, content);
+    });
 }
 
-function tailNewestLog(name, grep, excludeGrep, socket) {
+function grepFile(path, fromPattern, toPattern, grep, excludeGrep, cb) {
+    var failed = false;
+    var fileStream = fs.createReadStream(path);
+    var stream = byline.createStream(fileStream);
+    var lines = [];
+    var r = new RegExp(grep);
+    fileStream.on('error', function (error) {
+        failed = true;
+        stream.removeAllListeners();
+        console.log('[yog2-agent] log', path, 'not found');
+        return cb && cb(null, []);
+    });
+    stream.on('readable', function () {
+        var line;
+        while (null !== (line = stream.read())) {
+            line = line.toString();
+            var matched = !grep || r.test(line);
+            if ((excludeGrep && !matched) || (!excludeGrep && matched)) {
+                lines.push(line);
+            }
+        }
+    });
+    stream.on('end', function () {
+        console.log('[yog2-agent] grep log', path);
+        cb && cb(null, lines);
+    });
+}
+
+function getFilesBetweenTime(filePath, from, to) {
+    var files = [];
+    var fromTimestamp = from.getTime();
+    var toTimestamp = to.getTime();
+    for (var i = fromTimestamp; i <= toTimestamp;) {
+        var searchingFile = path.join(ROOT_PATH, strftime(filePath, new Date(i)));
+        files.push(searchingFile);
+        if (i === toTimestamp) {
+            break;
+        }
+        i += 60 * 60 * 1000;
+        if (i > toTimestamp) {
+            i = toTimestamp;
+        }
+    };
+    return files;
+}
+
+function tailNewestLog(name, grep, excludeGrep, eventID, socket) {
     var now = new Date();
     var conf = logMap[name];
     var logPath = path.join(ROOT_PATH, strftime(conf.path, now));
@@ -69,7 +144,10 @@ function tailNewestLog(name, grep, excludeGrep, socket) {
             onTruncate: 'reset',
             endOnError: true
         });
-        var eventID = name + '#tail';
+        stream.on('error', function (error) {
+            stream.removeAllListeners();
+            console.log('[yog2-agent] log', logPath, 'not found');
+        });
         console.log('[yog2-agent] open log', logPath, 'with tail');
         var prevChunk = '';
         stream.on('data', function (data) {
